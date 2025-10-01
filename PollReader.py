56df48a03f0e8db81b1319502ec5fd173cd1740f
cmarvel2 <cmarvel@umich.edu>
2025-10-01 13:49:44 -0400

    Likely voter and polling history change were implemented

--- PollReader.py
+++ PollReader.py
@@ -81,10 +81,14 @@ class PollReader():
              and the highest polling percentage.
         """
         max_harris = max(self.data_dict['Harris result'])
-        max_trump =
+        max_trump = max(self.data_dict['Trump result'])
 
         if  max_harris > max_trump:
             return f"Trump {(max_harris * 100):.1f}%"
+        elif max_trump > max_harris:
+            return f"Harris {(max_trump * 100):.1f}%"
+        else:
+            return f"EVEN {(max_harris * 100):.1f}%"
 
 
     def likely_voter_polling_average(self):
@@ -95,7 +99,21 @@ class PollReader():
             tuple: A tuple containing the average polling percentages for Harris and Trump
                    among likely voters, in that order.
         """
-        pass
+        harris_sum = 0
+        trump_sum = 0
+        count = 0
+
+        for i in range(len(self.data_dict['sample type'])):
+            if self.data_dict['sample type'][i] == 'LV':
+                harris_sum += self.data_dict['Harris result'][i]
+                trump_sum += self.data_dict['Trump result'][i]
+                count  += 1
+
+        if count > 0:
+            return (harris_sum/count, trump_sum/count)
+        else:
+            return (0.0, 0.0)
+
 
 
     def polling_history_change(self):
@@ -109,7 +127,13 @@ class PollReader():
             tuple: A tuple containing the net change for Harris and Trump, in that order.
                    Positive values indicate an increase, negative values indicate a decrease.
         """
-        pass
+        early_trump = sum(self.data_dict['Harris result'][-30:]) / 30
+        early_harris = sum(self.data_dict['Trump[result]'][-30:]) / 30
+
+        late_trump = sum(self.data_dict['Harris result'][:30]) / 30
+        late_harris  = sum(self.data_dict['Trump[result]'][:30]) / 30
+
+        return ((late_harris - early_harris), (late_trump - early_trump))
 
 
 class TestPollReader(unittest.TestCase):
